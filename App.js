abb7c323d80df6e86b5fd7950efa434f944b9d77
lucaskenji <ra1957066@gmail.com>
2021-08-08 20:53:36 -0300

    Revertendo App.js pro estado original

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import 'react-native-gesture-handler';
 import React  from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Rotas from './Componentes/Layout/Rotas';
-import FormLogin from './Componentes/PaginaInicial/FormLogin';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <FormLogin/>
-      {/*<Rotas/>*/}
+      <Rotas/>
     </NavigationContainer>
   );
 }
