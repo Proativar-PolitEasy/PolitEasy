0b9ad91ddb2e5768ae7fd196f6a503f0087ba36d
lucaskenji <ra1957066@gmail.com>
2021-08-15 02:04:44 -0300

    Implementado conexão e classe Pergunta

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React  from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Rotas from './Componentes/Layout/Rotas';
+import Pergunta from './lib/Pergunta';
 
 export default function App() {
   return (
