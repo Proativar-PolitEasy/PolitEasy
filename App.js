ae256d248a4ccc670aa408b20dabfda4aea62fc3
lucaskenji <ra1957066@gmail.com>
2021-08-16 12:03:50 -0300

    Implementando classe Pergunta

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import 'react-native-gesture-handler';
 import React  from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Rotas from './Componentes/Layout/Rotas';
-import Pergunta from './lib/Pergunta';
 
 export default function App() {
   return (
