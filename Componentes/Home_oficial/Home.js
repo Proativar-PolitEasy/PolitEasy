1caba4d522515e768ca597e769b967aba1c18c5d
unknown <Administrador@LDI307.ctibauru.feb.unesp.br>
2021-11-08 10:21:53 -0300

    Obter noticias por API

--- Componentes/Home_oficial/Home.js
+++ Componentes/Home_oficial/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, ImageBackground, Image } from 'react-native';
 import { } from '@expo/vector-icons';
 import Base from '../Barra_nav/Barra';
+import axios from 'axios';
 
 export default function Home_oficial({route, navigation}) {
+    const [noticias, setNoticias] = useState([]);
+    
+    useEffect(() => {
+        // Obter notícias pela API da Google News
+        axios.get("api_url")
+        .then((respostaApi) => {
+            setNoticias(respostaApi["articles"]);
+        })
+        .catch((erroApi) => {
+            console.log(erroApi);
+        })
+    }, []);
+    
     return (
         <ImageBackground source={require('../../assets/bg.jpg')} style={{flex:1, resizeMode:'cover'}}>
             <View style={{flex:1,}}>
@@ -18,6 +32,20 @@ export default function Home_oficial({route, navigation}) {
                             </View>
                         </View>
                         <View style={{height:'40%', alignItems:'center'}}>
+                            
+                            {
+                                noticias.map(noticia =>
+                                    <View style={{width:'100%', height:'50%', flexDirection:'column', alignItems:'center', justifyContent:'space-around'}}>
+                                        <View style={{width:70, height:70, backgroundColor:'#16abb2', borderRadius:70/2, alignItems:'center', justifyContent:'center'}}>
+                                            <View style={{width:60, height:60, backgroundColor:'grey', borderRadius:99}}>
+                                                { noticia["title"] }
+                                            </View>
+                                        </View>
+                                    </View>
+                                )
+                            }
+                            
+                            { /* [OLD] NOTÍCIAS
                             <View style={{width:'100%', height:'50%', flexDirection:'row', alignItems:'center', justifyContent:'space-around'}}>
                                 <View style={{width:70, height:70, backgroundColor:'#16abb2', borderRadius:70/2, alignItems:'center', justifyContent:'center'}}>
                                     <View style={{width:60, height:60, backgroundColor:'grey', borderRadius:99}}></View>
@@ -46,6 +74,9 @@ export default function Home_oficial({route, navigation}) {
                                     <View style={{width:60, height:60, backgroundColor:'grey', borderRadius:99}}></View>
                                 </View>
                             </View>
+                            */ }
+
+
                         </View>
                     </View>
                 </View>
